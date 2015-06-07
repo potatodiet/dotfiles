85f548f5f6e5d566ba5e2786b2ae019b20a14e56
Justin Harrison <justin@matthin.com>
2015-06-07 15:03:10 -0400

    Compile ruby with readline support

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # Install applications
-sudo apt-get install vim git libssl-dev
+sudo apt-get install vim git libssl-dev libreadline-dev
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
@@ -15,7 +15,7 @@ echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 source ~/.bashrc
 if ! rbenv versions | grep -q 2.2.2; then
-  rbenv install 2.2.2
+  CONFIGURE_OPTS="--with-readline-dir=/usr/include/readline" rbenv install 2.2.2
   rbenv global 2.2.2
 fi
 
