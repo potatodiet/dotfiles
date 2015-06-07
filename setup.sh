92356230231e7644555294c5cdeb984ced4ad5c2
Justin Harrison <justin@matthin.com>
2015-06-07 14:29:08 -0400

    Install ruby using rbenv

--- setup.sh
+++ setup.sh
@@ -1,10 +1,21 @@
 # Install applications
-sudo apt-get install vim git
+sudo apt-get install vim git libssl-dev
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 # The actual dotfiles
 ln -s -f $(realpath .vimrc) $(realpath ~/.vimrc)
 
 # Initialize any programs
 vim +PluginInstall +qall
+
+# rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+if ! rbenv versions | grep -q 2.2.2; then
+  rbenv install 2.2.2
+  rbenv global 2.2.2
+fi
 
