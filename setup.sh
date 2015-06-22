b16a15e0babbc276f83c9bcc59989ec942d13fc3
Justin Harrison <justin@matthin.com>
2015-06-22 13:49:26 -0400

    Use nvm to manage node installs

--- setup.sh
+++ setup.sh
@@ -21,6 +21,12 @@ if ! rbenv versions | grep -q 2.2.2; then
   rbenv global 2.2.2
 fi
 
+# nvm
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.25.4/install.sh | bash
+source ~/.nvm/nvm.sh
+nvm install stable
+nvm alias default stable
+
 # Fonts
 mkdir ~/.fonts
 cd /tmp
