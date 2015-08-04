9f1a83dfd922018dc4ee7efc732eb86fa59f7dc8
Justin Harrison <justin@matthin.com>
2015-08-03 21:39:25 -0400

    YCM depends on CMake

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 # Install applications
-sudo apt-get install vim git libssl-dev libreadline-dev doxygen
+sudo apt-get install vim git libssl-dev libreadline-dev doxygen cmake
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
 git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
