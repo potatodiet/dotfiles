566fd966febaf92195f7bdafbb32f4488e71dfdd
Justin Harrison <justin@matthin.com>
2016-03-24 10:23:01 -0400

    Forgot to clone zsh plugin

--- zsh.sh
+++ zsh.sh
@@ -1,7 +1,8 @@
 sudo apt-get install zsh -y
 chsh -s /bin/zsh
 
+git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
+
 touch ~/.zshrc
 ln -s -f $(realpath .zshrc) $(realpath ~/.zshrc)
-
 source ~/.zshrc
