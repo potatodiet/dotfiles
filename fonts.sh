17a41833d60325b933f47476ddfd3cba1eed1fa1
Justin Harrison <justin@matthin.com>
2016-05-19 16:22:56 -0400

    No need for fonts / view

--- fonts.sh
+++
@@ -1,6 +0,0 @@
-mkdir ~/.fonts
-cd /tmp
-wget https://github.com/adobe-fonts/source-code-pro/archive/1.017R.tar.gz
-tar -xzf 1.017R.tar.gz
-mv source-code-pro-1.017R/TTF/SourceCodePro-Regular.ttf ~/.fonts/SourceCodePro-Regular.ttf
-# I need to find a way to automatically set terminl font.
--- view.sh
+++
@@ -1,3 +0,0 @@
-gsettings set com.canonical.indicator.datetime time-format "'custom'"
-# Clock example: 2016-02-19, 13:50:62
-gsettings set com.canonical.indicator.datetime custom-time-format "'%Y-%m-%d, %I:%M:%S'"
