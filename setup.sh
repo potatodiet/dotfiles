675dcdd5cb742fa05847282faccee8c5b9d3fe08
Justin Harrison <justin@matthin.com>
2016-06-21 07:39:25 -0400

    Track bundler's config

--- setup.sh
+++ setup.sh
@@ -16,3 +16,10 @@ source ./rbenv.sh
 source ./nvm.sh
 source ./cpp.sh
 
+if [ ! -f ~/.bundle/config ]; then
+  # ~/.bundle probably won't exist
+  mkdir -p ~/.bundle
+
+  ln -sfn $(readlink -f .bundle/config) $(readlink -f ~/.bundle/config)
+fi
+
